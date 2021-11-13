0265c39c13d271f73f6f547b68a465a89fbbe1f2
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-13 06:02:38 -0500

    Connecting to fakestoreapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Badge, Drawer, Grid, LinearProgress } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+import { useQuery } from 'react-query'
+
+import { Wrapper } from './StyledComponents/App.styles'
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  decsription: string;
+  image: string;
+  price: number;
+  title: string;
+}
+
+const getProducts = async () => {
+  await (await fetch('https://fakestoreapi.com/products')).json();
+}
 
 const App = () => {
+  
+  
   return (
     <div>
       App
